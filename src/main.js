d1649a768659104e4b1d114c94caf05453a797e4
Cory Hayes <hayescor@oregonstate.edu>
2020-02-26 09:04:57 -0800

    Added background images for map. Reverted back to 16 pixel knight to fix transparency issue. Procedural map generation does not work.

--- src/main.js
+++ src/main.js
@@ -7,6 +7,12 @@ const input_flag_left = 8;
 const input_flag_special = 16;
 const input_flag_exit = 32;
 
+// tile dimenstions
+const tile_dim = 16;
+
+// 2D array initialized in genMap to store map
+var tiles;
+
 // init canvas object
 var stage = new createjs.Stage("game_board");
 
@@ -95,12 +101,15 @@ document.addEventListener("keyup", (event) => {
 
 // Setup Function for Setting Up State Object
 function setup(){
-	var spriteWidth = 64;
-	var spriteHeight = 64;
+	var knightImg = new Image(196, 16);                             // width, height of resized knight image
+	knightImg.src = "assets/images/knight_run_spritesheet.png";
+	
+	var spriteWidth = 16;
+	var spriteHeight = 16;
 	
 	// knight run spritesheet data
 	var knightSheet = {
-		images: ["assets/images/knight_run_spritesheet.png"],
+		images: [knightImg],
 		frames: {width: spriteWidth, height: spriteHeight},
 		framerate: 15
 	};
@@ -124,6 +133,105 @@ function setup(){
     }
 
     return state;
+}
+
+// create map of size X tiles width, Y tiles height
+function gen_map(x, y){
+	// random number of rooms between 3 - 4
+	const room_count = Math.floor(Math.random() * 2 + 3);
+	
+	// room tile count variability +-5%
+	const room_margin = 0.05;
+	
+	const tile_count = x  * y;
+	const tile_colors = [
+		"floor_1.png", 
+		"floor_2.png", 
+		"floor_9.png", 
+		"floor_side4.png"
+	];
+	
+	var tilesRemaining = tile_count;
+	var tilesPerRoom = [];
+	
+	tiles = [];
+	
+	// initialize tiles 2D array
+	for(var i = 0; i < x; i++){
+		tiles[i] = [];
+	}
+	
+	// allocate first (room_count - 1) rooms an amount of tiles
+	for(var i = 0; i < room_count - 1; i++){
+		tilesPerRoom[i] = tile_count / room_count + Math.floor((Math.random() * 2 * room_margin - room_margin) * tile_count);
+		tilesRemaining = tilesRemaining - tilesPerRoom[i];
+	}
+	
+	// allocate last room remaining tiles
+	tilesPerRoom[i - 1] = tilesRemaining;
+	
+	for(var i = 0; i < room_count; i++){
+		var startTileX = 0;
+		var startTileY = 0;
+		
+		while(tiles[startTileX][startTileY] != null){
+			startTileX = Math.floor(Math.random() * x);
+			startTileY = Math.floor(Math.random() * y);
+		}
+		
+		create_tiles(tilesPerRoom[i], tile_colors[i], startTileX, startTileY, x, y);
+	}
+}
+
+function create_tiles(tileCount, color, x, y, max_x, max_y){
+	if(x >= 0 && x < max_x && y >= 0 && y < max_y && tileCount > 0){
+		if(tiles[x][y] == null){
+			tiles[x][y] = {
+				color: this.color,
+				x: this.x * tile_dim,
+				y: this.y * tile_dim,
+			};
+			
+			create_tiles(--tileCount, color, --x, --y, max_x, max_y);
+			create_tiles(--tileCount, color, --x, ++y, max_x, max_y);
+			create_tiles(--tileCount, color, ++x, --y, max_x, max_y);
+			create_tiles(--tileCount, color, ++x, ++y, max_x, max_y);
+		}
+		else if(tiles[x][y].color != this.color){
+			tiles[x][y].color = "wall_1.png";
+		}
+		else{
+			return;
+		}
+	}
+	else{
+		return;
+	}
+}
+
+function draw_map(){
+	const assets_dir = "assets/images/";
+	/* Bad tiles instantiation. Some tiles are empty
+	for(var i = 0; i < tiles.length; i++){
+		for(var j = 0; j < tiles[i].length; j++){
+			console.log(i, j);
+			var newTile = new createjs.Bitmap(assets_dir + tiles[i][j].color);
+			
+			newTile.x = tiles[i][j].x;
+			newTile.y = tiles[i][j].y;
+			stage.addChild(newTile);
+		}
+	}
+	*/
+	for(var i = 0; i < stage.canvas.width / tile_dim; i++){
+		for(var j = 0; j < stage.canvas.height / tile_dim; j++){
+			var newTile = new createjs.Bitmap(assets_dir + "floor_1.png");
+			
+			newTile.x = i * tile_dim;
+			newTile.y = j * tile_dim;
+			stage.addChild(newTile);
+		}
+	}
 }
 
 /* Systems -- Resolves */
@@ -193,9 +301,11 @@ function update(event){
 
 function main(){
 	state = setup();
+	gen_map((stage.canvas.width / tile_dim), (stage.canvas.height / tile_dim));
+	draw_map();
 	
 	stage.addChild(state.player.animation);
-	createjs.Ticker.setFPS(40);
+	createjs.Ticker.setFPS(45);
     createjs.Ticker.addEventListener("tick", update);
 }
 
