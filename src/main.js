1d0fad5b450064d89d136d6e90d97c7b63bd22f3
HaofengTian <32280884+HaofengTian@users.noreply.github.com>
2020-02-26 22:02:25 -0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,25 +1,21 @@
 // Input Flag Enum
 const input_flag_none = 0;
-const input_flag_up1 = 1;
-const input_flag_right1 = 2;
-const input_flag_down1 = 4;
-const input_flag_left1 = 8;
-const input_flag_special1 = 16;
+const input_flag_up = 1;
+const input_flag_right = 2;
+const input_flag_down = 4;
+const input_flag_left = 8;
+const input_flag_special = 16;
 const input_flag_exit = 32;
-const input_flag_up2 = 64;
-const input_flag_right2 = 128;
-const input_flag_down2 = 256;
-const input_flag_left2 = 512;
-const input_flag_special2 = 1024;
-
-// tile dimenstions
-const tile_dim = 16;
-
-// 2D array initialized in genMap to store map
-var tiles;
 
 // init canvas object
 var stage = new createjs.Stage("game_board");
+var hw = stage.canvas.width / 2;
+var hh = stage.canvas.height / 2;
+var sp;  //test block
+sp = new createjs.Shape();
+sp.graphics.beginFill("rgba(254,241,103,1)").drawRect(hw,hh,40,40)
+stage.addChild(sp);
+//object();
 
 // stores game state
 var state;
@@ -30,51 +26,27 @@ document.addEventListener("keydown", (event) => {
     var key_code_temp = event.keyCode;
     var string_conv_temp = String.fromCharCode(key_code_temp);
 	
-	//start player1 run animation
-	if(string_conv_temp == "W" || string_conv_temp == "D" || string_conv_temp == "S" || string_conv_temp == "A"){
-		if(state.player1.isMoving == false){
-			state.player1.animation.gotoAndPlay(0);
-			state.player1.isMoving = true;
-		}
-	}
-	
-	//start player2 run animation
-	if(string_conv_temp == "I" || string_conv_temp == "L" || string_conv_temp == "K" || string_conv_temp == "J"){
-		if(state.player2.isMoving == false){
-			state.player2.animation.gotoAndPlay(0);
-			state.player2.isMoving = true;
-		}
+	//start player run animation
+	if(state.player.isMoving == false){
+		state.player.animation.gotoAndPlay(0);
+		state.player.isMoving = true;
 	}
 	
     // Key Definitions A->B Encoding
     switch(string_conv_temp){
         case "W":
-            input_buffer_down = input_buffer_down | input_flag_up1;
+            input_buffer_down = input_buffer_down | input_flag_up;
             break;
         case "D":
-            input_buffer_down = input_buffer_down | input_flag_right1;
-			state.player1.facingRight = true;
+            input_buffer_down = input_buffer_down | input_flag_right;
+			state.player.facingRight = true;
             break;
         case "S":
-            input_buffer_down = input_buffer_down | input_flag_down1;
+            input_buffer_down = input_buffer_down | input_flag_down;
             break;
         case "A":
-            input_buffer_down = input_buffer_down | input_flag_left1;
-			state.player1.facingRight = false;
-            break;
-		case "I":
-            input_buffer_down = input_buffer_down | input_flag_up2;
-            break;
-        case "L":
-            input_buffer_down = input_buffer_down | input_flag_right2;
-			state.player2.facingRight = true;
-            break;
-        case "K":
-            input_buffer_down = input_buffer_down | input_flag_down2;
-            break;
-        case "J":
-            input_buffer_down = input_buffer_down | input_flag_left2;
-			state.player2.facingRight = false;
+            input_buffer_down = input_buffer_down | input_flag_left;
+			state.player.facingRight = false;
             break;
         default:
             console.log(key_code_temp);
@@ -95,47 +67,25 @@ document.addEventListener("keyup", (event) => {
     var key_code_temp = event.keyCode;
     var string_conv_temp = String.fromCharCode(key_code_temp);
 	
-	//stop player1 run animation
-	if(string_conv_temp == "W" || string_conv_temp == "D" || string_conv_temp == "S" || string_conv_temp == "A"){
-		if(state.player1.isMoving == true){
-			state.player1.animation.gotoAndStop(0);
-			state.player1.isMoving = false;
-		}
-	}
-	
-	//stop player2 run animation
-	if(string_conv_temp == "I" || string_conv_temp == "L" || string_conv_temp == "K" || string_conv_temp == "J"){
-		if(state.player2.isMoving == true){
-			state.player2.animation.gotoAndStop(0);
-			state.player2.isMoving = false;
-		}
+	//stop player run animation
+	if(state.player.isMoving == true){
+		state.player.animation.gotoAndStop(0);
+		state.player.isMoving = false;
 	}
 	
     // Key Definitions A->B Encoding
     switch(string_conv_temp){
         case "W":
-            input_buffer_down = input_buffer_down & ~input_flag_up1;
+            input_buffer_down = input_buffer_down & ~input_flag_up;
             break;
         case "D":
-            input_buffer_down = input_buffer_down & ~input_flag_right1;
+            input_buffer_down = input_buffer_down & ~input_flag_right;
             break;
         case "S":
-            input_buffer_down = input_buffer_down & ~input_flag_down1;
+            input_buffer_down = input_buffer_down & ~input_flag_down;
             break;
         case "A":
-            input_buffer_down = input_buffer_down & ~input_flag_left1;
-            break;
-		case "I":
-            input_buffer_down = input_buffer_down & ~input_flag_up2;
-            break;
-        case "L":
-            input_buffer_down = input_buffer_down & ~input_flag_right2;
-            break;
-        case "K":
-            input_buffer_down = input_buffer_down & ~input_flag_down2;
-            break;
-        case "J":
-            input_buffer_down = input_buffer_down & ~input_flag_left2;
+            input_buffer_down = input_buffer_down & ~input_flag_left;
             break;
         default:
             break;
@@ -152,36 +102,24 @@ document.addEventListener("keyup", (event) => {
 
 // Setup Function for Setting Up State Object
 function setup(){
-	var knightImg = new Image(196, 16);                             // width, height of resized knight image
-	knightImg.src = "assets/images/knight_run_spritesheet.png";
-	
-	var spriteWidth = 16;
-	var spriteHeight = 16;
+	var spriteWidth = 64;
+	var spriteHeight = 64;
 	
 	// knight run spritesheet data
 	var knightSheet = {
-		images: [knightImg],
+		images: ["assets/images/knight_run_spritesheet.png"],
 		frames: {width: spriteWidth, height: spriteHeight},
 		framerate: 15
 	};
-	
-	// create sprites	
+
+	// create sprite	
 	var knightSprite = new createjs.SpriteSheet(knightSheet);
-	var animation1 = new createjs.Sprite(knightSprite);
-	var animation2 = new createjs.Sprite(knightSprite);
+	var animation = new createjs.Sprite(knightSprite);
 	
     // setup player data
     var state = {
-		player1: {
-			animation: animation1,
-			isMoving: false,
-			facingRight: true,
-			facingRightPrev: true,
-			width: spriteWidth,
-			height: spriteHeight
-		},
-		player2: {
-			animation: animation2,
+		player: {
+			animation: animation,
 			isMoving: false,
 			facingRight: true,
 			facingRightPrev: true,
@@ -193,105 +131,6 @@ function setup(){
     }
 
     return state;
-}
-
-// create map of size X tiles width, Y tiles height
-function gen_map(x, y){
-	// random number of rooms between 3 - 4
-	const room_count = Math.floor(Math.random() * 2 + 3);
-	
-	// room tile count variability +-5%
-	const room_margin = 0.05;
-	
-	const tile_count = x  * y;
-	const tile_colors = [
-		"floor_1.png", 
-		"floor_2.png", 
-		"floor_9.png", 
-		"floor_side4.png"
-	];
-	
-	var tilesRemaining = tile_count;
-	var tilesPerRoom = [];
-	
-	tiles = [];
-	
-	// initialize tiles 2D array
-	for(var i = 0; i < x; i++){
-		tiles[i] = [];
-	}
-	
-	// allocate first (room_count - 1) rooms an amount of tiles
-	for(var i = 0; i < room_count - 1; i++){
-		tilesPerRoom[i] = tile_count / room_count + Math.floor((Math.random() * 2 * room_margin - room_margin) * tile_count);
-		tilesRemaining = tilesRemaining - tilesPerRoom[i];
-	}
-	
-	// allocate last room remaining tiles
-	tilesPerRoom[i - 1] = tilesRemaining;
-	
-	for(var i = 0; i < room_count; i++){
-		var startTileX = 0;
-		var startTileY = 0;
-		
-		while(tiles[startTileX][startTileY] != null){
-			startTileX = Math.floor(Math.random() * x);
-			startTileY = Math.floor(Math.random() * y);
-		}
-		
-		create_tiles(tilesPerRoom[i], tile_colors[i], startTileX, startTileY, x, y);
-	}
-}
-
-function create_tiles(tileCount, color, x, y, max_x, max_y){
-	if(x >= 0 && x < max_x && y >= 0 && y < max_y && tileCount > 0){
-		if(tiles[x][y] == null){
-			tiles[x][y] = {
-				color: this.color,
-				x: this.x * tile_dim,
-				y: this.y * tile_dim,
-			};
-			
-			create_tiles(--tileCount, color, --x, --y, max_x, max_y);
-			create_tiles(--tileCount, color, --x, ++y, max_x, max_y);
-			create_tiles(--tileCount, color, ++x, --y, max_x, max_y);
-			create_tiles(--tileCount, color, ++x, ++y, max_x, max_y);
-		}
-		else if(tiles[x][y].color != this.color){
-			tiles[x][y].color = "wall_1.png";
-		}
-		else{
-			return;
-		}
-	}
-	else{
-		return;
-	}
-}
-
-function draw_map(){
-	const assets_dir = "assets/images/";
-	/* Bad tiles instantiation. Some tiles are empty
-	for(var i = 0; i < tiles.length; i++){
-		for(var j = 0; j < tiles[i].length; j++){
-			console.log(i, j);
-			var newTile = new createjs.Bitmap(assets_dir + tiles[i][j].color);
-			
-			newTile.x = tiles[i][j].x;
-			newTile.y = tiles[i][j].y;
-			stage.addChild(newTile);
-		}
-	}
-	*/
-	for(var i = 0; i < stage.canvas.width / tile_dim; i++){
-		for(var j = 0; j < stage.canvas.height / tile_dim; j++){
-			var newTile = new createjs.Bitmap(assets_dir + "floor_1.png");
-			
-			newTile.x = i * tile_dim;
-			newTile.y = j * tile_dim;
-			stage.addChild(newTile);
-		}
-	}
 }
 
 /* Systems -- Resolves */
@@ -306,46 +145,37 @@ function resolve_player(){
         return; // Reduce Workload
     }
 
-	// Player one moves
     // Move Up
-    if(state.input & input_flag_up1){
-        state.player1.animation.y -= 2;
+    var direction;
+    if(state.input & input_flag_up){
+        direction = "up";
+        if (!collision(direction)) {
+            state.player.animation.y -= 2;
+        }
     }
 
     // Move Right
-    if(state.input & input_flag_right1){
-        state.player1.animation.x += 2;
+    if(state.input & input_flag_right){
+        direction = "right";
+        if (!collision(direction)) {
+            state.player.animation.x += 2;
+        }
     }
 
     // Move Down
-    if(state.input & input_flag_down1){
-        state.player1.animation.y += 2;
+    if (state.input & input_flag_down) {
+        direction = "down";
+        if (!collision(direction)) {
+            state.player.animation.y += 2;
+        }
     }
 
     // Move Left
-    if(state.input & input_flag_left1){
-        state.player1.animation.x -= 2;
-    }
-	
-	// Player two moves
-	// Move Up
-    if(state.input & input_flag_up2){
-        state.player2.animation.y -= 2;
-    }
-
-    // Move Right
-    if(state.input & input_flag_right2){
-        state.player2.animation.x += 2;
-    }
-
-    // Move Down
-    if(state.input & input_flag_down2){
-        state.player2.animation.y += 2;
-    }
-
-    // Move Left
-    if(state.input & input_flag_left2){
-        state.player2.animation.x -= 2;
+    if (state.input & input_flag_left) {
+        direction = "left";
+        if (!collision(direction)) {
+            state.player.animation.x -= 2;
+        }
     }
 }
 
@@ -358,17 +188,10 @@ function resolve_exit(){
 // flip sprite to face proper direction
 function rotate_player(){
 	// turn sprite if necessary
-	if(state.player1.facingRight != state.player1.facingRightPrev){
-		state.player1.animation.scaleX = state.player1.animation.scaleX * -1;
-		state.player1.animation.x -= state.player1.animation.scaleX * state.player1.width;
-		state.player1.facingRightPrev = state.player1.facingRight;
-	}
-	
-	// turn sprite if necessary
-	if(state.player2.facingRight != state.player2.facingRightPrev){
-		state.player2.animation.scaleX = state.player2.animation.scaleX * -1;
-		state.player2.animation.x -= state.player2.animation.scaleX * state.player2.width;
-		state.player2.facingRightPrev = state.player2.facingRight;
+	if(state.player.facingRight != state.player.facingRightPrev){
+		state.player.animation.scaleX = state.player.animation.scaleX * -1;
+		state.player.animation.x -= state.player.animation.scaleX * state.player.width;
+		state.player.facingRightPrev = state.player.facingRight;
 	}
 }
 
@@ -390,13 +213,89 @@ function update(event){
 
 function main(){
 	state = setup();
-	gen_map((stage.canvas.width / tile_dim), (stage.canvas.height / tile_dim));
-	draw_map();
 	
-	stage.addChild(state.player1.animation);
-	stage.addChild(state.player2.animation);
+	stage.addChild(state.player.animation);
 	createjs.Ticker.setFPS(40);
     createjs.Ticker.addEventListener("tick", update);
 }
 
+
+
+//function object() {
+//    var hw = stage.canvas.width / 2;
+//    var hh = stage.canvas.height / 2;
+//    sp = new createjs.Shape();
+//    sp.graphics.beginFill("rgba(254,241,103,1)").drawRect(hw,hh,40,40)
+//    stage.addChild(sp);
+//}
+
+
+function collision(direction) {
+
+   var is_blocked = false;
+   var area = new createjs.Shape();
+   area.graphics.beginStroke("rgba(255,0,0,0.5)").drawRect(0, 0, state.player.width, state.player.height);
+   area.x = state.player.animation.x;
+   area.y = state.player.animation.y;
+
+    //sp.x = 400;
+   // sp.y = 400;
+   // sp.width = 10;
+   // sp.height = 10;
+   // area.width = 10;
+   // area.height = 10;
+
+    console.log(area.x, area.y, state.player.animation.x, state.player.animation.y);
+    console.log(sp.x, sp.y);
+    
+    switch (direction) {
+        case "up":
+            area.y = area.y - 2;
+            break;
+        case "down":
+            area.y = area.y + 2;
+            break;
+        case "left":
+            area.x = area.x - 2;
+            break;
+        case "right":
+            area.x = area.x + 2;
+            break;
+    }
+     
+
+
+        //(x1 + w1) < x2 || (x2 + w2) < x1 || (y1 + h1) < y2 || (y2 + h2) < y1
+        //x,y is the position of left up corner of the square. w=width h=height
+
+        //if ((area.x + area.width * 0.5) < sp.x - 0.5 * sp.width ||
+        //    sp.x + sp.width * 0.5 < area.x - area.width * 0.5 ||
+        //    (area.y + area.height * 1.5) < sp.y + 0.5 * sp.height ||
+         //   sp.y + sp.height * 1.5 < area.y + area.height * 0.5) { 
+
+
+    //need to figure out how to get postion of cp(test yellow cube), use hard code for test
+    if ((area.x + 10 * 0.5) < 200 - 0.5 * 10 ||
+                200 + 10 * 0.5 < area.x - 10 * 0.5 ||
+                (area.y + 10 * 1.5) < sp.y + 0.5 * 10 ||
+                200 + 10 * 1.5 < area.y + 10 * 0.5) { 
+
+        is_blocked = false;
+        }
+
+        else
+        {
+            is_blocked = true;
+        }
+
+
+    console.log(is_blocked, area.width, sp.width, area.height,sp.height);
+    return is_blocked;
+}
+
+
 main();
+
+
+
+
